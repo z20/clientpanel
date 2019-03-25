55cbac54716906d8526b7c87f16fe7ad5f329cfc
Zach Holdren <zholdren20@gmail.com>
2019-03-24 20:43:03 -0700

    saving work - need to define onDeleteClick

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Client } from '../../models/Client';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance = false;
+  showBalanceUpdateInput = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client != null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+
+      this.client = client;
+    });
   }
 
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -37,4 +37,19 @@ export class ClientService {
   newClient(client: Client) {
     this.clientCollection.add(client);
   }
+
+  getClient(id: string): Observable<Client> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges().map(action => {
+      if (action.payload.exists === false) {
+        return null;
+      } else {
+        const data = action.payload.data() as Client;
+        data.id = action.payload.id;
+        return data;
+      }
+    });
+
+    return this.client;
+  }
 }
