3888b4b18c98ba6e68958a4021022d95f9227c36
Zach Holdren <zholdren20@gmail.com>
2019-05-21 17:58:55 -0700

    Fix typos in edit-client

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  };
 
-  constructor() { }
+// tslint:disable-next-line: no-inferrable-types
+  disableBalanceOnEdit: boolean = true;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => this.client =
+      client);
+  }
+
+  onSubmit({value, valid}: {value: Client, valid: boolean}) {
+
   }
 
 }
